6f44a2a9eb52499f48c5a6cb53fc5b4db36b823d
mitchGlad07 <mgla0008@student.monash.edu>
2025-03-29 18:40:23 +1100

    New page and following guide for improvements

--- main.js
+++ main.js
@@ -1,2 +1,11 @@
 "use strict";
 
+
+// Function to occur when a button is pressed, removes option icons from the display
+// Currently as template only
+function ClearOptions() {
+    var x = document.getElementsByClassName("option");
+    for (var i = 0; i < x.length; i++) {
+      x[i].style.display = "none";
+    }
+}
