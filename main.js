72d155c8a983d6a500df5d676040c1e4797366c4
mitchGlad07 <m.gladigau07@gmail.com>
2025-03-30 20:40:13 +1100

    Beginning Hover animation

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 "use strict";
 
+// Current goal to display images with text underneath, with the images enlargening on hover of the mouse
+// Each image should link to the same page, scrolling to that projects section
 
 // Function to occur when a button is pressed, removes option icons from the display
 // Currently as template only
